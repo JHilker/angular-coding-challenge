44fb0e9d375882d3a8073da3c017caf46fa6174d
Jacob Hilker <hilker.j@gmail.com>
2015-03-20 10:18:35 -0400

    Implement sorting by columns; finally get around to testing the angular

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -8,38 +8,44 @@ angular.module('angularCodingChallengeApp')
     $scope.currentPage = 0;
     $scope.perPage = 10;
 
-    $scope.orderBy = 'lastName';
-    $scope.sortOrder = 'asc';
+    // true means asc for _.sortByOrder
+    $scope.sortOrder = true;
+    $scope.sortField = 'lastName';
 
-    $http.get('/api/users').success(function(users) {
+
+    $http.get('/api/users').success(function (users) {
       $scope.users = users;
     });
 
-    $scope.editUser = function(user) {
+    $scope.editUser = function (user) {
       $scope.cachedUsers[$scope.users.indexOf(user)] = angular.copy(user);
       user.editing = true;
     };
 
-
-    $scope.cancel = function(user) {
+    $scope.cancel = function (user) {
       var index = $scope.users.indexOf(user);
       if (user._id) {
-        $scope.users[index] = $scope.cachedUsers[index]
+        $scope.users[index] = $scope.cachedUsers[index];
       } else {
         $scope.users.splice(index, 1);
       }
     };
 
-    $scope.addUserRow = function() {
+    $scope.addUserRow = function () {
       $scope.users.unshift({ editing: true });
     };
 
-    $scope.sortUsers = function(field) {
-      $scope.users = _.sortBy($scope.users, field);
+    $scope.sortUsers = function (field) {
+      if ($scope.sortField === field) {
+        $scope.sortOrder = !$scope.sortOrder;
+      } else {
+        $scope.sortField = field;
+        $scope.sortOrder = true;
+      }
     };
 
 
-    $scope.saveUser = function(user) {
+    $scope.saveUser = function (user) {
       if (user.editing !== false) {
         user.lastEdited = moment();
         if (user._id) {
@@ -53,7 +59,7 @@ angular.module('angularCodingChallengeApp')
       }
     };
 
-    $scope.addUser = function() {
+    $scope.addUser = function () {
       if($scope.newUser === '') {
         return;
       }
@@ -61,25 +67,30 @@ angular.module('angularCodingChallengeApp')
       $scope.newUser = '';
     };
 
-    $scope.deleteUser = function(user) {
+    $scope.deleteUser = function (user) {
       $http.delete('/api/users/' + user._id);
     };
 
-    $scope.visibleUsers = function() {
-      return $scope.users.slice($scope.currentPage * $scope.perPage, $scope.currentPage * $scope.perPage + 10);
+    $scope.sortedUsers = function () {
+      return _.sortByOrder($scope.users, $scope.sortField, $scope.sortOrder);
     };
 
-    $scope.visiblePageRange = function() {
+    $scope.visibleUsers = function () {
+      return $scope.sortedUsers().slice($scope.currentPage * $scope.perPage,
+                                        $scope.currentPage * $scope.perPage + 10);
+    };
+
+    $scope.visiblePageRange = function () {
       var firstUser = $scope.currentPage * $scope.perPage + 1;
       var lastUser = (firstUser + 9) > $scope.users.length ? $scope.users.length : (firstUser + 9);
       return firstUser + '-' + lastUser;
-    }
+    };
 
-    $scope.previousPage = function() {
+    $scope.previousPage = function () {
       $scope.currentPage -= 1;
-    }
+    };
 
-    $scope.nextPage = function() {
+    $scope.nextPage = function () {
       $scope.currentPage += 1;
     };
   });
--- client/app/main/main.controller.spec.js
+++ client/app/main/main.controller.spec.js
@@ -25,4 +25,81 @@ describe('Controller: MainCtrl', function () {
     $httpBackend.flush();
     expect(scope.users.length).toBe(2);
   });
+
+  describe('sorting', function () {
+    it('should have default values for sortOrder and sortField', function () {
+      expect(scope.sortOrder).toBe(true);
+      expect(scope.sortField).toBe('lastName');
+    });
+
+    describe('sortUsers', function () {
+      it('should set sortField if it differs from the current sortField and set sortOrder to asc', function () {
+        scope.sortField = 'lastName';
+        scope.sortOrder = false;
+
+        scope.sortUsers('age');
+        expect(scope.sortField).toBe('age');
+        expect(scope.sortOrder).toBe(true);
+      });
+
+      it('should set inverse sortOrder when the orderField is unchanged', function () {
+        scope.sortField = 'lastName';
+        scope.sortOrder = true;
+
+        scope.sortUsers('lastName');
+        expect(scope.sortField).toBe('lastName');
+        expect(scope.sortOrder).toBe(false);
+
+        scope.sortUsers('lastName');
+        expect(scope.sortField).toBe('lastName');
+        expect(scope.sortOrder).toBe(true);
+      });
+    });
+
+    describe('sortedUsers', function () {
+      it('should return an array of users sorted using sortField and sortOrder', function () {
+        scope.users = [{ firstName: 'Jacob' }, { firstName: 'Test' }];
+        scope.sortField = 'firstName';
+        scope.sortOrder = true;
+
+        expect(scope.sortedUsers().length).toBe(2);
+        expect(scope.sortedUsers()[0].firstName).toBe('Jacob');
+        expect(scope.sortedUsers()[1].firstName).toBe('Test');
+
+        scope.sortField = 'firstName';
+        scope.sortOrder = false;
+
+        expect(scope.sortedUsers().length).toBe(2);
+        expect(scope.sortedUsers()[0].firstName).toBe('Test');
+        expect(scope.sortedUsers()[1].firstName).toBe('Jacob');
+      });
+    });
+  });
+
+  describe('pagination', function () {
+    beforeEach(function () {
+      scope.users = _.times(22, function () {
+        return { firstName: 'testUser' };
+      });
+    });
+    it('should have default values for currentPage and perPage', function () {
+      expect(scope.currentPage).toBe(0);
+      expect(scope.perPage).toBe(10);
+    });
+
+    describe('visibleUsers', function () {
+      it('should return the proper number of users per page', function () {
+        expect(scope.users.length).toBe(22);
+        scope.currentPage = 0;
+        expect(scope.visibleUsers().length).toBe(10);
+
+        scope.currentPage = 1;
+        expect(scope.visibleUsers().length).toBe(10);
+
+        scope.currentPage = 2;
+        expect(scope.visibleUsers().length).toBe(2);
+      });
+    });
+
+  });
 });
